27df88c999a44a035bce6ab425d607e22b01c945
Marcos <marcos.mfd04@gmail.com>
2019-10-12 17:27:42 -0400

    activity mostrar estudiantes

--- app/src/main/java/com/itla/prueba_db/MostrarEstudiante.java
+++ app/src/main/java/com/itla/prueba_db/MostrarEstudiante.java
@@ -5,24 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.itla.prueba_db.entidad.Estudiante;
+import com.itla.prueba_db.repositorio.EstudianteRepositorio;
+import com.itla.prueba_db.repositorio.EstudianteRepositorioDbImpl;
+
+import java.util.List;
 
 public class MostrarEstudiante extends AppCompatActivity {
+    EstudianteRepositorio estudianteRepositorio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mostrar_estudiante);
 
-        Button btn = (Button)findViewById(R.id.btnAtras);
+        estudianteRepositorio = new EstudianteRepositorioDbImpl(getBaseContext());
 
-        btn.setOnClickListener(new View.OnClickListener(){
+        List<Estudiante> estudiantes = estudianteRepositorio.buscar();
+        ListView lv = findViewById(R.id.listEstudiante);
+        // adapter para presentar los elementos
+        lv.setAdapter(new ArrayAdapter<>(getBaseContext(),R.layout.support_simple_spinner_dropdown_item,estudiantes.toArray()));
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),MainActivity.class);
-                startActivityForResult(intent,0);
-            }
-        });
+
     }
 }
--- app/src/main/java/com/itla/prueba_db/entidad/Estudiante.java
+++ app/src/main/java/com/itla/prueba_db/entidad/Estudiante.java
@@ -29,4 +29,9 @@ public class Estudiante {
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
